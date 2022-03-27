163bdbf15b684661da23c974493cfbd742ce4bcd
araceli gonzales <aragonzales.k@gmail.com>
2022-03-27 13:21:44 -0700

    fixing based on lesson snaoshot

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-var commandLineArgs = process.argv;
-//console.log(commandLineArgs);
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-//oconsole.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
 
 const printProfileData = profileDataArr => {
     for (let i = 0; i < profileDataArr.length; i+= 1){
         console.log(profileDataArr[i]);
     }
-    //console.log(profileDataArr)
+
     console.log('================');
+    //is the same as this 
     profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 printProfileData(profileDataArgs)
