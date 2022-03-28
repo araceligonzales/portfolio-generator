581f3c7a3c02b94c762e5af4ea9b8d0a8021d5ef
araceli gonzales <aragonzales.k@gmail.com>
2022-03-27 17:24:39 -0700

    modularize code

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
+const fs = require('fs')
+
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i+= 1){
-        console.log(profileDataArr[i]);
-    }
+const [name, github] = profileDataArgs;
 
-    console.log('================');
-    //is the same as this 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-printProfileData(profileDataArgs)
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if(err) throw new Error (err);
+
+    console.log('Portfolio is complete! Check out the index.html to see the output!');
+});
